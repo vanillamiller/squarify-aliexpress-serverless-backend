19df6b3da9792c4304a1865cd49b1282df3682d1
anthonymiller1 <anthonym1@student.unimelb.edu.au>
2020-02-26 19:12:50 +1100

    changed

--- serverless-functions/auth/encryption.js
+++ serverless-functions/auth/encryption.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const masterkeyBuffer = Buffer.from("tesing", 'hex');
+const masterkeyBuffer = Buffer.from(process.env.MASTER_KEY, 'hex');
 
 module.exports = {
     /**
